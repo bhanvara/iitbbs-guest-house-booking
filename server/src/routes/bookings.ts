a15f8926b154f2d71ee862f988c8207ae24ecabd
Kumar Saurabh <india.kumar.saurabh@hotmail.com>
2024-03-29 23:13:01 +0530

    --

---
+++ server/src/routes/bookings.ts
@@ -0,0 +1,16 @@
+// GET /bookings - Retrieve all bookings
+// POST /bookings - Create a new booking
+// GET /bookings/:id - Retrieve a specific booking by Booking ID
+// PUT /bookings/:id - Update a specific booking by Booking ID
+// DELETE /bookings/:id - Delete a specific booking by Booking ID
+// Rooms
+
+// .
+// ├── app.js
+// └── routes
+//     ├── students.js
+//     ├── facultyStaff.js
+//     ├── bookings.js
+//     ├── rooms.js
+//     ├── login.js
+//     └── gymkhana.js
---
+++ server/src/routes/rooms.ts
@@ -0,0 +1,5 @@
+// GET /rooms - Retrieve all rooms
+// POST /rooms - Create a new room
+// GET /rooms/:id - Retrieve a specific room by Room Number
+// PUT /rooms/:id - Update a specific room by Room Number
+// DELETE /rooms/:id - Delete a specific room by Room Number
