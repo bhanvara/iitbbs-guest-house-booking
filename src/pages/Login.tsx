b3fe8ff42bc3c9922832807c1d22a3f63a7c94ee
Kumar Saurabh <india.kumar.saurabh@hotmail.com>
2024-04-14 22:06:29 +0530

    Refactor Login component to use button for login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,11 +4,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Auth = () => {
   const { loginWithRedirect } = useAuth0();
 
-  useEffect(() => {
-    loginWithRedirect();
-  }, [loginWithRedirect]);
-
-  return null;
+  return (
+    <div>
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+    </div>
+  );
 };
 
 export default Auth;
