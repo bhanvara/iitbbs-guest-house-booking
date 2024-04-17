a45eecfc3a8f07c6c0411e1134cad45f92374d8e
Kumar Saurabh <india.kumar.saurabh@hotmail.com>
2024-04-17 13:08:01 +0530

    Update ApproveBookings.tsx to fetch pending approvals with authorization token

--- src/pages/ApproveBookings.tsx
+++ src/pages/ApproveBookings.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import RequestComponent from './components/RequestComponent';
+import { useAuth0 } from '@auth0/auth0-react';
 
 interface Request {
   bookingID: string,
@@ -23,25 +24,41 @@ export default function ApproveBookings() {
 
   const [pendingRequests, setPendingRequests] = useState<Request[]>([]);
   const [activeButton, setActiveButton] = useState("pending");
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     // Fetching list of booking IDs for a supervisor ID
     const fetchPendingApprovals = async () => {
+      const token = await getAccessTokenSilently();
       try {
-        const response = await fetch('http://localhost:3001/api/users/pendingApprovals/f1');
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api/users/pendingApprovals/f1`,{
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
         const data = await response.json();
         // data.Pending_Bookings contains an array of booking IDs
         // Fetching details for each booking ID
         const requests = await Promise.all(data.Pending_Bookings.map(async (bookingId: number) => {
-          const bookingResponse = await fetch(`http://localhost:3001/api/bookings/getDetails/${bookingId}`);
+          const bookingResponse = await fetch(`http://localhost:3001/api/bookings/getDetails/${bookingId}`,{
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
           const bookingData = await bookingResponse.json();
 
           const checkinDate = new Date(bookingData.details[0].Check_In_Date).toLocaleDateString('en-GB').split('/').join('/');
           const checkoutDate = new Date(bookingData.details[0].Check_Out_Date).toLocaleDateString('en-GB').split('/').join('/');
 
           // http://localhost:3001/api/bookings/roomDetails/?roomID=4
-          const roomDetailsAPI:any = await fetch(`http://localhost:3001/api/bookings/roomDetails/?roomID=${bookingData.details[0].Room_ID}`);
+          const roomDetailsAPI:any = await fetch(`http://localhost:3001/api/bookings/roomDetails/?roomID=${bookingData.details[0].Room_ID}`,{
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          });
           const roomDetails = await roomDetailsAPI.json();
+
+          console.log("token is:", localStorage.getItem('token'));
 
           return {
             bookingID: bookingData.details[0].Booking_ID,
@@ -68,7 +85,7 @@ export default function ApproveBookings() {
     };
 
     fetchPendingApprovals();
-  }, []);
+  }, [user, isAuthenticated, getAccessTokenSilently]);
 
   return (
     <div className="h-full" style={{ backgroundColor: '#edeff0' }}>
